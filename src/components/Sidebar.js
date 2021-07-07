f8d38dfc72b4e0c847d136696e13bb1bf1c1c7da
Rahul Krishan <rahul@funnelll.com>
2021-07-07 16:36:24 -0700

    implemented navbar with home, about, and products links

---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sidebar = () => {
+
+    return (
+        <>
+        <h1>Sidebar</h1>
+        </>
+    )
+}
+
+export default Sidebar;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+import ErrorPage from "./ErrorPage";
+
+export { HomePage, AboutPage, ErrorPage };
